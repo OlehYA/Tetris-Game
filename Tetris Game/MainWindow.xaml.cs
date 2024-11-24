d02b988ec66742a22f43190c773b367e1a290d01
OlehYa <olehyakuts@gmail.com>
2024-11-24 13:13:29 +0200

    Setting Up the Canvas

--- Tetris Game/MainWindow.xaml.cs
+++ Tetris Game/MainWindow.xaml.cs
@@ -44,9 +44,38 @@ namespace Tetris_Game
             new BitmapImage(new Uri("Assets/Block-Z.png", UriKind.Relative)),
         };
 
+        private readonly Image[,] imageControls;
+
+        private GameState gameState = new GameState();
+
         public MainWindow()
         {
             InitializeComponent();
+            imageControls = SetupGameCanvas(gameState.GameGrid);
+        }
+
+        private Image[,] SetupGameCanvas(GameGrid grid)
+        {
+            Image[,] imageControls = new Image[grid.Rows, grid.Columns];
+            int cellSize = 25;
+
+            for(int r =0; r< grid.Rows; r++)
+            {
+                for(int c =0; c<grid.Columns; c++)
+                {
+                    Image imageControl = new Image
+                    {
+                        Width = cellSize,
+                        Height = cellSize
+                    };
+
+                    Canvas.SetTop(imageControl, (r -2) * cellSize);
+                    Canvas.SetLeft(imageControl, c * cellSize);
+                    GameCanvas.Children.Add(imageControl);
+                    imageControls[r,c] = imageControl;
+                }
+            }
+            return imageControls;
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
